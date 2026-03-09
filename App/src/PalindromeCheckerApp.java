52ac5211ad1058d6d26d726e7a1556e2256cf67b
rudra <rudragandhi19gmail.com>
2026-03-09 15:47:35 +0530

    Rudra Added UC1

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -1,4 +1,24 @@
-package PACKAGE_NAME;
-
 public class PalindromeCheckerApp {
+
+    public static void main(String[] args) {
+
+        // Display welcome message
+        System.out.println("=================================");
+        System.out.println("     Welcome to PalindromeChecker");
+        System.out.println("=================================");
+
+        // Display application information
+        System.out.println("Application Name : PalindromeChecker App");
+        System.out.println("Version          : 1.0");
+
+        // Startup message
+        System.out.println("---------------------------------");
+        System.out.println("Application started successfully.");
+        System.out.println("Ready to check palindromes...");
+        System.out.println("---------------------------------");
+
+        // Program will continue to the next use case
+        // (Palindrome checking logic will go here later)
+
+    }
 }
