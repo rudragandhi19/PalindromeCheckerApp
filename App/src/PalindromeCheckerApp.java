f54a892444b15252db295b14a952b4734ad350d8
Bhumika Dugar <bhumikadugar440@gmail.com>
2026-03-16 15:57:43 +0530

    rudra Added usecase9

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -1,84 +1,31 @@
 public class PalindromeCheckerApp {
 
-    // Node class for Singly Linked List
-    static class Node {
-        char data;
-        Node next;
+    // Recursive method to check palindrome
+    public static boolean isPalindrome(String text, int start, int end) {
 
-        Node(char data) {
-            this.data = data;
-            this.next = null;
-        }
-    }
-
-    // Method to check palindrome
-    public static boolean isPalindrome(Node head) {
-
-        if (head == null || head.next == null) {
+        // Base condition
+        if (start >= end) {
             return true;
         }
 
-        Node slow = head;
-        Node fast = head;
-
-        // Find middle using fast and slow pointers
-        while (fast != null && fast.next != null) {
-            slow = slow.next;
-            fast = fast.next.next;
+        // Compare start and end characters
+        if (text.charAt(start) != text.charAt(end)) {
+            return false;
         }
 
-        // Reverse second half
-        Node prev = null;
-        Node current = slow;
-
-        while (current != null) {
-            Node next = current.next;
-            current.next = prev;
-            prev = current;
-            current = next;
-        }
-
-        // Compare first half and reversed second half
-        Node firstHalf = head;
-        Node secondHalf = prev;
-
-        while (secondHalf != null) {
-            if (firstHalf.data != secondHalf.data) {
-                return false;
-            }
-            firstHalf = firstHalf.next;
-            secondHalf = secondHalf.next;
-        }
-
-        return true;
-    }
-
-    // Convert string to linked list
-    public static Node createLinkedList(String text) {
-        Node head = null;
-        Node tail = null;
-
-        for (char ch : text.toCharArray()) {
-            Node newNode = new Node(ch);
-            if (head == null) {
-                head = tail = newNode;
-            } else {
-                tail.next = newNode;
-                tail = newNode;
-            }
-        }
-        return head;
+        // Recursive call
+        return isPalindrome(text, start + 1, end - 1);
     }
 
     public static void main(String[] args) {
 
         String text = "madam";
 
-        // Convert string to linked list
-        Node head = createLinkedList(text);
+        // Call recursive function
+        boolean result = isPalindrome(text, 0, text.length() - 1);
 
-        // Check palindrome
-        if (isPalindrome(head)) {
+        // Display result
+        if (result) {
             System.out.println("The string \"" + text + "\" is a palindrome.");
         } else {
             System.out.println("The string \"" + text + "\" is not a palindrome.");
